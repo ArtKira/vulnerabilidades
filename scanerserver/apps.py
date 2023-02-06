ebd76c250c5f2551893e26b01e5721bd3bc4a81e
Arturo Sanchez <thegamerslol2.zw@gmail.com>
2023-02-05 18:37:41 -0600

    version 1 vulnerabilidades de servidores, se importa nmap

---
+++ scanerserver/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ScanerserverConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'scanerserver'
---
+++ scanerserver/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+import nmap
+
+#from django.http import HttpResponse
+
+# Create your views here.
+def scan(request):#definimos las funciones a ejecutar
+    return render(request, 'scan.html')
+
+def result(request):#aqui resive la direccion IP para su escaneo
+    host=request.GET.get('host')
+    nm=nmap.PortScanner()
+    resultado=nm.scan(host)
+
+    statehost="Host : %s" % (host)#muestra el estado 
+    state="State : %s" % nm[host].state()#estado up 
+    for proto in nm[host].all_protocols():
+        protocolo='Protocol : %s' % proto#indica el protocolo TCP
+
+        lport = nm[host][proto].keys()
+        sorted(lport)
+    for port in lport:#recorre todos los puertos y los muestra, muestra su estado 
+        r='port : %s\tstate : %s' % (port, nm[host][proto][port]['state'])
+        return render(request, 'result.html', {'host':statehost, 'resultado': r, 'estado':state, 'protocolo':protocolo})#retorna todos los valores
