735e3236b9717cc85afd87eac247a134460f978a
Arturo Sanchez <thegamerslol2.zw@gmail.com>
2023-02-16 18:42:43 -0600

    solucion de error al no reconocer ningun puerto abierto

--- scanerserver/views.py
+++ scanerserver/views.py
@@ -8,18 +8,21 @@ def scan(request):#definimos las funciones a ejecutar
     return render(request, 'scan.html')
 
 def result(request):#aqui resive la direccion IP para su escaneo
-    host=request.GET.get('host')
-    nm=nmap.PortScanner()
-    resultado=nm.scan(host,  arguments='-T5')
+    try:
+        host=request.GET.get('host')
+        nm=nmap.PortScanner()
+        resultado=nm.scan(host,  arguments='-T5')
 
-    statehost="Host : %s" % (host)#muestra el estado 
-    state="State : %s" % nm[host].state()#estado up 
-    for proto in nm[host].all_protocols():
-        protocolo='Protocol : %s' % proto#indica el protocolo TCP
+        statehost="Host : %s" % (host)#muestra el estado 
+        state="State : %s" % nm[host].state()#estado up 
+        for proto in nm[host].all_protocols():
+            protocolo='Protocol : %s' % proto#indica el protocolo TCP
 
-        lport = nm[host][proto].keys()
-        sorted(lport)
-    r = []
-    for port in lport:#recorre todos los puertos y los muestra, muestra su estado 
-        r.append('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))#se añade los resultados
-    return render(request, 'result.html', {'host':statehost, 'resultado': r, 'estado':state, 'protocolo':protocolo})#retorna todos los valores
+            lport = nm[host][proto].keys()
+            sorted(lport)
+        r = []
+        for port in lport:#recorre todos los puertos y los muestra, muestra su estado 
+            r.append('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))#se añade los resultados
+        return render(request, 'result.html', {'host':statehost, 'resultado': r, 'estado':state, 'protocolo':protocolo})#retorna todos los valores
+    except:
+        return render(request, 'nullport.html', {'host':statehost, 'estado':state})
